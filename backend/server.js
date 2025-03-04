870528b2ee39ae52b64323c6b25098f776a83723
Suhail7985 <suhailmansoori7985@gmail.com>
2025-03-04 16:19:16 +0530

    ats-score-update

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors());
 // Connect to MongoDB
 connectDB();
 
-app.use(cors({ origin: "http://localhost:3000" })); 
+app.use(cors({ origin: "https://ats-score-checker.onrender.com" })); 
 app.use((req, res, next) => {
   console.log(`🔥 Received request: ${req.method} ${req.url}`);
   next();
